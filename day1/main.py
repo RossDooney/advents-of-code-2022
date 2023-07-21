61f25b757ad310a828c34ed12cdf4e7b6815b70a
RossDooney <rossdooney@googlemail.com>
2023-07-22 00:13:58 +0100

    day one done

--- day1/main.py
+++ day1/main.py
@@ -1,2 +1,17 @@
 import os
 
+
+f = open("calories.txt", "r")
+count = 0
+biggest_elf = 0
+current_elf = 0
+for line in f:
+    count += 1
+    if line != "\n":
+        current_elf += int(line)
+    else:
+        if biggest_elf < current_elf:
+            biggest_elf = current_elf
+        current_elf = 0
+
+print(biggest_elf)
