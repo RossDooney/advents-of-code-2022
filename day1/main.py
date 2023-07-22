8c5f26d62f861c9f17d5e4761cb797a61e9207ba
RossDooney <rossdooney@googlemail.com>
2023-07-22 23:40:46 +0100

    apparently there was a part 2

--- day1/main.py
+++ day1/main.py
@@ -1,17 +1,24 @@
 import os
+import numpy as np
 
 
 f = open("calories.txt", "r")
 count = 0
-biggest_elf = 0
+biggest_elf = np.array([0, 0, 0])
 current_elf = 0
+total_calories = 0
 for line in f:
     count += 1
     if line != "\n":
         current_elf += int(line)
     else:
-        if biggest_elf < current_elf:
-            biggest_elf = current_elf
+        if biggest_elf[0] < current_elf:
+            biggest_elf[0] = current_elf
+            biggest_elf = np.sort(biggest_elf)[::1]
         current_elf = 0
 
-print(biggest_elf)
+for i in range(0, len(biggest_elf)):
+    total_calories = total_calories + biggest_elf[i]
+
+
+print(total_calories)
