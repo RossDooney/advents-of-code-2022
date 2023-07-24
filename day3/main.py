ce2db698b536bd311d1ef517288a6a9ef5a7c775
RossDooney <rossdooney@googlemail.com>
2023-07-25 00:54:31 +0100

    day3 part 2, correct answer, needs work to stop loop correctly

--- day3/main.py
+++ day3/main.py
@@ -1,26 +1,33 @@
 import os
 
-#find overlappying char and use ord() to find its value. Then subtract ord('a') + 1 or ord('A') + 27
+# find overlappying char and use ord() to find its value. Then subtract ord('a') + 1 or ord('A') + 27
 
-f = open("input.txt", "r")
+# f = open("input.txt", "r")
 
-first_part =""
-second_part=""
+# first_part = ""
+# second_part = ""
+# score is 2639,the while loop does one to many iterations causing an error, could resolve by just using first solution with a 3rd var, but this feels closer to a good solution. Will rewrite later
 common_char = ""
 score = 0
+count = 0
+
+with open("input.txt", "r") as f:
+    line = True
+    while line:
+        line = (f.readline().rstrip(), f.readline().rstrip(), f.readline().strip())
+        common_char = set.intersection(*map(set, line)).pop()
+        if common_char.isupper() == True:
+            score += int(ord(common_char) - ord("A") + 27)
+        else:
+            score += int(ord(common_char) - ord("a") + 1)
+        print(score)
 
 
-for line in f:
-    first_part = line[:len(line)//2]
-    second_part = line[len(line)//2:]
-    common_char = set(first_part).intersection(second_part).pop()
-    if common_char.isupper() == True:
-        score += int(ord(common_char) - ord('A')+27)
-    else:
-        score += int(ord(common_char) - ord('a')+1)
+# for line in f:
+#     # common_char = set(first_part).intersection(second_part).pop()
+#     if common_char.isupper() == True:
+#         score += int(ord(common_char) - ord("A") + 27)
+#     else:
+#         score += int(ord(common_char) - ord("a") + 1)
 
 print(score)
-
-
-
-
